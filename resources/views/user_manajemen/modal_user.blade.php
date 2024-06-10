0793c4a8b4628b9e84e270390cabb6fa4d6004bf
soismeee <soismeee@gmail.com>
2024-06-10 19:24:34 +0700

    tes

--- resources/views/user_manajemen/modal_user.blade.php
+++ resources/views/user_manajemen/modal_user.blade.php
@@ -2,7 +2,7 @@
     <div class="modal-dialog">
         <div class="modal-content">
             <div class="modal-header">
-                <h5 class="modal-title" id="judulmodalPengguna">Judul modal</h5>
+                <h5 class="modal-title" id="judulModalPengguna">Judul modal</h5>
                 <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
             </div>
             <form id="form_user">
